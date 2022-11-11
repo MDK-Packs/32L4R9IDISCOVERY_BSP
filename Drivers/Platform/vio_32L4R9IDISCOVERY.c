c95d264c6822e896d5360de1462b02cb40535964
Ziga Mahne <ziga.mahne@arm.com>
2022-11-11 13:17:03 +0100

    Update VIO CMSIS-Driver
    - correct variables initialization to avoid compiler warnings

--- Drivers/Platform/vio_32L4R9IDISCOVERY.c
+++ Drivers/Platform/vio_32L4R9IDISCOVERY.c
@@ -1,11 +1,12 @@
 /******************************************************************************
  * @file     vio_32L4R9IDISCOVERY.c
  * @brief    Virtual I/O implementation for board 32L4R9IDISCOVERY
- * @version  V1.0.0
- * @date     22. September 2021
+ * @version  V1.1.0
+ * @date     9. November 2022
  ******************************************************************************/
 /*
- * Copyright (c) 2021 Arm Limited (or its affiliates). All rights reserved.
+ * Copyright (c) 2021-2022 Arm Limited (or its affiliates).
+ * All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -312,7 +313,7 @@ uint32_t x_size, y_size;
 #endif
 }
 
-// Print formated string to test terminal.
+// Print formatted string to test terminal.
 int32_t vioPrint (uint32_t level, const char *format, ...) {
   va_list args;
   int32_t ret;
@@ -386,6 +387,7 @@ void vioSetSignal (uint32_t mask, uint32_t signal) {
 
 #if !defined CMSIS_VOUT
   // Output signals to LEDs
+
   if ((mask & vioLED0) != 0U) {
     if ((signal & vioLED0) != 0U) {
       BSP_LED_On(LED_GREEN);
@@ -581,7 +583,7 @@ void vioSetIPv4 (uint32_t id, vioAddrIPv4_t addrIPv4) {
 // Get IPv4 address input.
 vioAddrIPv4_t vioGetIPv4 (uint32_t id) {
   uint32_t index = id;
-  vioAddrIPv4_t addrIPv4 = {0U, 0U, 0U, 0U};
+  vioAddrIPv4_t addrIPv4 = { {0U, 0U, 0U, 0U} };
 #if !defined CMSIS_VIN
 // Add user variables here:
 
@@ -625,8 +627,8 @@ void vioSetIPv6 (uint32_t id, vioAddrIPv6_t addrIPv6) {
 // Get IPv6 address input.
 vioAddrIPv6_t vioGetIPv6 (uint32_t id) {
   uint32_t index = id;
-  vioAddrIPv6_t addrIPv6 = {0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U,
-                            0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U};
+  vioAddrIPv6_t addrIPv6 = { {0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U,
+                              0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U} };
 #if !defined CMSIS_VIN
 // Add user variables here:
 
